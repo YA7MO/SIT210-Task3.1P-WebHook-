c40b94a1c45355c23a1164ca3647123152925a4b
Yahya Aloyoni <39294093+YA7MO@users.noreply.github.com>
2020-04-17 21:26:15 +1000

    Update motion.ino

--- motion.ino
+++ motion.ino
@@ -1,11 +1,11 @@
 int analogvalue = 0;
-int led2 = D7; 
+//int led2 = D7; 
 
 void setup() {
   // Subscribe to the integration response event
     pinMode(A0, INPUT);
     Particle.subscribe("hook-response/motion", myHandler, MY_DEVICES);
-    pinMode(led2, OUTPUT);
+    //pinMode(led2, OUTPUT);
 }
 
 void myHandler(const char *event, const char *data) {
@@ -17,11 +17,11 @@ void myHandler(const char *event, const char *data) {
 
 void loop() {
   // Get some data
-  analogvalue = analogRead(A0);
-  String data = String(analogvalue);
+  analogvalue = analogRead(A0); // save the detected motion 
+  String data = String(analogvalue); // stringfy the data 
 //   Trigger the integration
-  Particle.publish("motion", data, PRIVATE);
-  // Wait 60 seconds
+  Particle.publish("motion", data, PRIVATE); // publish data to the webhook
+  // Wait 30 seconds
   delay(30000);
   
 }
